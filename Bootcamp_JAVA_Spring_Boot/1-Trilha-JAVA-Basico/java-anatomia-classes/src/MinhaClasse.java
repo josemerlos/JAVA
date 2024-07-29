c7bf18be884b8b5864636bd59e5319b53ef09c97
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-07-28 21:47:29 -0300

    start

---
+++ Bootcamp_JAVA_Spring_Boot/1-Trilha-JAVA-Basico/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse { //classes as primeiras letras em maiusculo
+    
+    public static void main(String[] args) { // void quer dizer que so ira executar, nao ira me retornar nada
+            System.out.print ("Vida Longa!");
+    } 
+
+
+}
