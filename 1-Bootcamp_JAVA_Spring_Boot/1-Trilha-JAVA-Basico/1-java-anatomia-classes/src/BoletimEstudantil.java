4f96e518c2f3b9cfa7918c0f7b0460823629806f
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-07-30 06:49:26 -0300

    update

---
+++ 1-Bootcamp_JAVA_Spring_Boot/1-Trilha-JAVA-Basico/1-java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,30 @@
+public class BoletimEstudantil { // explicacoes sobre identacao
+    
+    public static void main(String[] args) { // digitar main + ctrl e espaco
+         
+        int mediaFinal = 6;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+
+
+
+      /*  nao identado
+            int mediaFinal = 6;
+            if(mediaFinal<6)	
+            System.out.println("REPROVADO"); 
+            else if(mediaFinal==6)
+            System.out.println("PROVA MINERVA"); 
+            else
+            System.out.println("APROVADO"); */
+
+
+    }
+
+
+
+
+}
--- 1-Bootcamp_JAVA_Spring_Boot/1-Trilha-JAVA-Basico/1-java-anatomia-classes/src/MinhaClasse.java
+++ 1-Bootcamp_JAVA_Spring_Boot/1-Trilha-JAVA-Basico/1-java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,7 @@
 public class MinhaClasse { //classes as primeiras letras em maiusculo
     
     public static void main(String[] args) { // void quer dizer que so ira executar, nao ira me retornar nada
-            System.out.print ("Vida Longa!");
+            //System.out.print ("Vida Longa!");
 
             String primeiroNome = "Jose";
             String segundoNome  = "Merlos";
