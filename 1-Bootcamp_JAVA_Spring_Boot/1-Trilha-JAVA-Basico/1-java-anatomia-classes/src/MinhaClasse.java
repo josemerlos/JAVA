6879d73f0a750f7103becdfdf956a020f0f84375
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-07-30 06:35:06 -0300

    update

--- 1-Bootcamp_JAVA_Spring_Boot/1-Trilha-JAVA-Basico/1-java-anatomia-classes/src/MinhaClasse.java
+++ 1-Bootcamp_JAVA_Spring_Boot/1-Trilha-JAVA-Basico/1-java-anatomia-classes/src/MinhaClasse.java
@@ -2,7 +2,30 @@ public class MinhaClasse { //classes as primeiras letras em maiusculo
     
     public static void main(String[] args) { // void quer dizer que so ira executar, nao ira me retornar nada
             System.out.print ("Vida Longa!");
+
+            String primeiroNome = "Jose";
+            String segundoNome  = "Merlos";
+
+            String nomeCompleto = nomeCompleto(primeiroNome, segundoNome); // utilizando metodo criado abaixo fora da main
+
+            System.out.print (nomeCompleto);
+
+
+
+                int idade = 23;
+                double altura = 1.62;
+             
+
+
     } 
+
+
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){ // mais uma classe alem da main - espera um retorno
+        return "Resultado do Método: " + primeiroNome.concat(" ").concat(segundoNome);
+
+
+    }
 
 
 }
