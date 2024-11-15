653ee1839d762fb31e759a48341349e2fb224a65
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-11-15 17:50:42 -0300

    Update LivroController.java

--- 2-Spring_Boot/2-CRUD-livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
+++ 2-Spring_Boot/2-CRUD-livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
@@ -6,14 +6,19 @@ import br.com.artigo.livro.entity.Livro;
 import br.com.artigo.livro.entity.Modelo;
 import br.com.artigo.livro.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller    //@Controller para o Spring encontrar a classe e fazer o gerenciamento para que ela receba requisições e envie a resposta ao usuário.
+@RestController    //para o Spring encontrar a classe e fazer o gerenciamento para que ela receba requisições e envie a resposta ao usuário.
+//To do método temos que anotar com @ResponseBody para indicar  ao Spring que nosso método não vai retornar uma página no response.
+//
+//Pensando em facilitar e eliminar anotações recorrentes, o Spring disponibiliza a anotação @RestController . Está anotação é uma contém as anotações @Controller e @ResponseBody em sua definição. Com isso,
+// ao anotarmos nossa classe com @RestController já estamos declarando para o Spring que nossa classe é um controller e que todos os seus métodos retornam um objeto. (automaticamente serializado em JSON no Java).
+@RequestMapping("/livros") //Para dizer qual é o endereço que o Spring vai chamar o nosso méto do listar, utilizamos a anotação @RequestMapping.
 public class LivroController {
 
     @Autowired//No Spring, para marcar os pontos de injeção dentro da sua classe, você utiliza a anotação @Autowired.
@@ -23,8 +28,8 @@ public class LivroController {
 
 
 
-    @ResponseBody //Como não queremos retornar uma página para o navegador, precisamos utilizar a anotação @ResponseBody. Ela é responsável por informa nosso @Controller que o objeto retornado é serializado automaticamente em JSON e passado de volta para o objeto HttpResponse.
-    @RequestMapping("/livros") //Para dizer qual é o endereço que o Spring vai chamar o nosso méto do listar, utilizamos a anotação @RequestMapping. Ela informa o Spring que o endereço /livros for chamado, o nosso métod o listar deve ser executado.
+    //@ResponseBody //Como não queremos retornar uma página para o navegador, precisamos utilizar a anotação @ResponseBody. Ela é responsável por informa nosso @Controller que o objeto retornado é serializado automaticamente em JSON e passado de volta para o objeto HttpResponse.
+    @GetMapping
     public List<Livro> listar(){
 
         List<Livro> livros = livroRepository.findAll();
@@ -34,9 +39,9 @@ public class LivroController {
     }
 
 
-    @ResponseBody
+    //@ResponseBody
     @Transactional //é usada para definir os requisitos da transação
-    @RequestMapping(path = "/livros", method = RequestMethod.POST)
+    @PostMapping
     public void salvar(@RequestBody Livro livro) { // recebe um livro como parametro para inserir na base de dados -- o request body para que forneca um objeto de dominio, permitindo a desserializacao automatica do objeto de entrada  em um objeto java, No caso, vamos receber um objeto JSON que reflete nossa entidade e desserializar para a entidade Livro (parâmetro de entrada do  métod
         livroRepository.save(livro); //chamamos o méto do save() que recebe nossa entidade (livro) como parâmetro e trata toda a lógica de negócio para persistir no banco.
 
@@ -44,20 +49,21 @@ public class LivroController {
 
 
 
-    @ResponseBody
+    //@ResponseBody
     @Transactional
-    @RequestMapping(path = "/livros", method = RequestMethod.PUT)
+    @PutMapping
     public void atualizar(@RequestBody Livro livro) {
         livroRepository.save(livro);
     }
 
 
-    @ResponseBody
+    //@ResponseBody
     @Transactional
-    @RequestMapping(path = "/livros/{isbn}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{isbn}")
     public void deletar(@PathVariable Long isbn){ //Na ação de deletar, a boa prática é passar a variável que representa nosso registro como parte da URL e não como um parâmetro da requisição. Portanto, utilizaremos a anotação PathVariabler
 
         livroRepository.deleteById(isbn);
+
 
     }
 
