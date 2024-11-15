31709c79ae5de598ce9cf392962e2f02fc19defa
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-11-15 10:16:38 -0300

    Update LivroRepository.java

--- 2-Spring_Boot/2-CRUD-livro/src/main/java/br/com/artigo/livro/repository/LivroRepository.java
+++ 2-Spring_Boot/2-CRUD-livro/src/main/java/br/com/artigo/livro/repository/LivroRepository.java
@@ -1,7 +1,11 @@
 package br.com.artigo.livro.repository;
 
-public interface LivroRepository {
-    
 
+import br.com.artigo.livro.entity.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository     //A anotação indica que a classe contém lógica de acesso a dados e instrui o Spring a converter as exceções específicas do banco para a hierarquia DataAccessException do Spring.
+public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
