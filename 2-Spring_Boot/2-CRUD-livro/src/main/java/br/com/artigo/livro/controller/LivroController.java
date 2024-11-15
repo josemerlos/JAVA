25163e467e17acf7540d97e25e5e2606d5bb2c9d
José Carlos Merlos Junior <carlosj.info@gmail.com>
2024-11-15 17:15:25 -0300

    Update LivroController.java

--- 2-Spring_Boot/2-CRUD-livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
+++ 2-Spring_Boot/2-CRUD-livro/src/main/java/br/com/artigo/livro/controller/LivroController.java
@@ -8,10 +8,7 @@ import br.com.artigo.livro.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,29 +16,52 @@ import java.util.List;
 @Controller    //@Controller para o Spring encontrar a classe e fazer o gerenciamento para que ela receba requisições e envie a resposta ao usuário.
 public class LivroController {
 
-    @Autowired
+    @Autowired//No Spring, para marcar os pontos de injeção dentro da sua classe, você utiliza a anotação @Autowired.
+    //Com esta anotação deixamos to do o trabalho de instanciar um objeto com o Spring. O framework puxa a responsabilidade e de alguma forma (interna) devolve a instância do nosso repositório.
+    //Não é apenas dar um “new”, até porque nosso LivroRepository é uma interface, não temos uma classe que implementa. Não temos lógica de negócio de banco
     private LivroRepository livroRepository;
 
 
 
     @ResponseBody //Como não queremos retornar uma página para o navegador, precisamos utilizar a anotação @ResponseBody. Ela é responsável por informa nosso @Controller que o objeto retornado é serializado automaticamente em JSON e passado de volta para o objeto HttpResponse.
-    @RequestMapping("/livros") //Para dizer qual é o endereço que o Spring vai chamar o nosso método listar, utilizamos a anotação @RequestMapping. Ela informa o Spring que o endereço /livros for chamado, o nosso método listar deve ser executado.
+    @RequestMapping("/livros") //Para dizer qual é o endereço que o Spring vai chamar o nosso méto do listar, utilizamos a anotação @RequestMapping. Ela informa o Spring que o endereço /livros for chamado, o nosso métod o listar deve ser executado.
     public List<Livro> listar(){
 
-        Livro livro = new Livro(123L, "Controller - Spring", 1, "Vitor", Categoria.INFORMATICA, Modelo.EBOOK);
-
-        return Arrays.asList(livro, livro, livro);
+        List<Livro> livros = livroRepository.findAll();
+        return livros;
 
 
     }
 
 
     @ResponseBody
-    @Transactional
+    @Transactional //é usada para definir os requisitos da transação
     @RequestMapping(path = "/livros", method = RequestMethod.POST)
     public void salvar(@RequestBody Livro livro) { // recebe um livro como parametro para inserir na base de dados -- o request body para que forneca um objeto de dominio, permitindo a desserializacao automatica do objeto de entrada  em um objeto java, No caso, vamos receber um objeto JSON que reflete nossa entidade e desserializar para a entidade Livro (parâmetro de entrada do  métod
-        livroRepository.save(livro);
+        livroRepository.save(livro); //chamamos o méto do save() que recebe nossa entidade (livro) como parâmetro e trata toda a lógica de negócio para persistir no banco.
 
     }
+
+
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros", method = RequestMethod.PUT)
+    public void atualizar(@RequestBody Livro livro) {
+        livroRepository.save(livro);
+    }
+
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros/{isbn}", method = RequestMethod.DELETE)
+    public void deletar(@PathVariable Long isbn){ //Na ação de deletar, a boa prática é passar a variável que representa nosso registro como parte da URL e não como um parâmetro da requisição. Portanto, utilizaremos a anotação PathVariabler
+
+        livroRepository.deleteById(isbn);
+
+    }
+
+
+
 
 }
